988400771a1372324aaff7fc093150e2085d0993
defendimattia <defendi.mattia92@gmail.com>
2025-04-09 16:18:57 +0200

    add: modify button

--- src/App.jsx
+++ src/App.jsx
@@ -14,31 +14,41 @@ function App() {
 
       const newArticle = { "id": list.length + 1, "title": inputArticle }
       setList([...list, newArticle])
-      console.log(list)
 
       setInputArticle("")
     }
 
   }
 
+
   function deleteFunction(idArticle) {
     const filteredList = list.filter(el => el.id !== idArticle)
     return setList(filteredList)
+  }
+
+  function modifyFunction(idArticle) {
+    const newText = prompt("inserisci nuovo testo")
+    const modList = list.map(el => el.id === idArticle ? {...el, title : newText} : el)
+    return setList(modList)
   }
 
   return (
     <>
       <h1>Lista articoli</h1>
       <ul>
-        {list.map(article => <li key={article.id}>
-          {article.title}
-          <button onClick={() => deleteFunction(article.id)}>Elimina</button>
-        </li>
+        {list.map(article =>
+          <li key={article.id}>
+            {article.title}
+            {/* delete button */}
+            <button onClick={() => deleteFunction(article.id)}>Elimina</button>
+            <button onClick={() => modifyFunction(article.id)}>Modifica</button>
+          </li>
         )}
       </ul>
       <hr />
       <form action="" onSubmit={submitFunction}>
         <input type="text" value={inputArticle} onChange={event => { setInputArticle(event.target.value) }} />
+        {/* add button  */}
         <button>Aggiungi</button>
       </form>
     </>
