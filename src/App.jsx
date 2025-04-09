8e9a84efe2b6ce794f9aae8ebebebfc0caa0d43f
defendimattia <defendi.mattia92@gmail.com>
2025-04-09 15:25:23 +0200

    add: milestrone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react"
 import articles from "./data/articles"
 
 function App() {
+
+  const [inputArticle, setInputArticle] = useState("")
+  const [list, setList] = useState(articles);
+
+
+  const submitFunction = event => {
+    event.preventDefault()
+
+    if (inputArticle) {
+      
+      const newArticle = { "id": list.length + 1, "title": inputArticle }
+      setList([...list, newArticle])
+      console.log(list)
+
+      setInputArticle("")
+    }
+
+  }
+
   return (
     <>
       <h1>Lista articoli</h1>
       <ul>
-        {articles.map(article => <li key={article.id}>{article.title}</li>)}
+        {list.map(article => <li key={article.id}>{article.title}</li>)}
       </ul>
       <hr />
-      <form action="">
-        <input type="text" />
+      <form action="" onSubmit={submitFunction}>
+        <input type="text" value={inputArticle} onChange={event => { setInputArticle(event.target.value) }} />
         <button>Aggiungi</button>
       </form>
     </>
